b934ea97d5f2edf842c33d8b60f486962441f48e
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2022-06-07 15:31:14 -0400

    0.5

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="minitorch",
+    version="1.0",
+    author="anshrad",
+    description="a minimal neural network libary",
+    packages=["minitorch"],
+)
